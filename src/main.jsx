b8b0554d2e7552a985b17257efd8c3064e1421f7
Caioscg <cacazaok5@gmail.com>
2023-03-22 20:47:23 -0300

    fragments

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/details'
+import { Details } from './pages/Details' // carrega o index
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
---
+++ src/pages/Details/index.jsx
@@ -0,0 +1,13 @@
+import "./styles.css"
+
+export function Details() {
+
+
+
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <span>Caio Gonzaga</span>
+    </>
+  )
+}
