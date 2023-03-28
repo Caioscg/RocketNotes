497f398e8bebfe6354a37c88be19882e478b53b7
Caioscg <cacazaok5@gmail.com>
2023-03-28 19:46:36 -0300

    buttonText

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -4,12 +4,15 @@ import { Header } from '../../components/header'
 import { Button } from '../../components/button'
 import { Section } from '../../components/Section'
 import { Tag } from '../../components/Tag'
+import { ButtonText } from '../../components/ButtonText'
 
 export function Details() {
 
   return (
     <Container>
       <Header />
+
+      <ButtonText title="Excluir nota" />
       
       <Section title="Links úteis">
         <Links>
