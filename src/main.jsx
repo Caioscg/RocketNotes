d08d4df123edc3bff8c3fa990e2c4fa92f2401d1
Caioscg <cacazaok5@gmail.com>
2023-03-29 17:48:05 -0300

    home grid

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Details } from './pages/Details' // carrega o index
+import { Home } from './pages/Home' // carrega o index
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from './styles/global'
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
