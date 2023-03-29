c47991651140b6bf14dfef5c4392e262d55351fd
Caioscg <cacazaok5@gmail.com>
2023-03-29 16:05:53 -0300

    details page

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 
 import { Header } from '../../components/header'
 import { Button } from '../../components/button'
@@ -12,21 +12,40 @@ export function Details() {
     <Container>
       <Header />
 
-      <ButtonText title="Excluir nota" />
-      
-      <Section title="Links úteis">
-        <Links>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-          <li><a href="#">https://www.rocketseat.com.br/</a></li>
-        </Links>
-      </Section>
+      <main>
+        <Content>
 
-      <Section title="Marcadores">
-        <Tag title="express" />
-        <Tag title="nodejs" />
-      </Section>
+          <ButtonText title="Excluir nota" />
+          
+          <h1>Introdução ao React</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Laborum provident fuga quibusdam non exercitationem, 
+            dicta iste alias dolore accusamus quod quas beatae? 
+            Deleniti ad eos expedita aut eaque a ipsam?
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+            Blanditiis, laborum minus in deleniti quos quae fugiat harum commodi, 
+            consequatur, culpa pariatur nam! Earum, voluptatem amet? 
+            Quisquam est iste in corporis.
+          </p>
 
-      <Button title="Voltar"/>
+          <Section title="Links úteis">
+            <Links>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+              <li><a href="#">https://www.rocketseat.com.br/</a></li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="nodejs" />
+          </Section>
+
+          <Button title="Voltar"/>
+
+        </Content>
+      </main>
+
     </Container>
   )
 }
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -8,7 +8,13 @@ export const Container = styled.div`
     grid-template-rows: 105px auto;
     grid-template-areas:
     "header"
-    "content"
+    "content";
+
+    > main {
+        grid-area: content;
+        overflow-y: scroll;
+        padding: 64px 0;
+    }
 `
 export const Links = styled.ul`
     list-style: none;
@@ -19,5 +25,28 @@ export const Links = styled.ul`
         a {
             color: ${({ theme }) => theme.COLORS.WHITE}
         }
+    }
+`
+
+export const Content = styled.div`
+    max-width: 550px;
+    margin: 0 auto;
+
+    display: flex;
+    flex-direction: column;
+
+    > button:first-child {
+        align-self: flex-end;
+    }
+
+    > h1 {
+        font-size: 36px;
+        font-weight: 500;
+        margin-top: 64px;
+    }
+
+    > p {
+        margin-top: 16px;
+        text-align: justify;
     }
 `
