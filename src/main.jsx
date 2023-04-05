4c80117c175a78f6afee4fbb8cb4012d3ca5e86d
Caioscg <cacazaok5@gmail.com>
2023-04-05 19:33:25 -0300

    sign up

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home' // carrega o index
 import { Details } from './pages/Details'
 import { SignIn } from './pages/SignIn'
+import { SignUp } from './pages/SignUp'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from './styles/global'
@@ -13,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
-      <SignIn />
+      <SignUp />
     </ThemeProvider>
   </React.StrictMode>,
 )
