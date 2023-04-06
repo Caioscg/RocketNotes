c83d2802799b60fcd3c086aab66f6f754aa12353
Caioscg <cacazaok5@gmail.com>
2023-04-06 16:27:22 -0300

    profile

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Home } from './pages/Home' // carrega o index
 import { Details } from './pages/Details'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
+import { Profile } from './pages/Profile'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from './styles/global'
@@ -14,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
-      <SignUp />
+      <Profile />
     </ThemeProvider>
   </React.StrictMode>,
 )
