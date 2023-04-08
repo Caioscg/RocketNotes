65be3abc8b3c2df29f7f540ea28e2b1bd31a43ea
Caioscg <cacazaok5@gmail.com>
2023-04-08 17:55:01 -0300

    new notes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import { Home } from './pages/Home' // carrega o index
 import { Details } from './pages/Details'
 import { SignIn } from './pages/SignIn'
 import { SignUp } from './pages/SignUp'
 import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from './styles/global'
@@ -15,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={Theme}>
       <GlobalStyles />
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )
---
+++ src/pages/New/index.jsx
@@ -0,0 +1,11 @@
+import { Header } from "../../components/header";
+
+import { Container } from "./styles";
+
+export function New() {
+    return(
+        <Container>
+            <Header />
+        </Container>
+    )
+}
