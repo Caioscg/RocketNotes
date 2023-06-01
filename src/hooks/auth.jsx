ac17232ebdf122c185b056248c25f17a6320ef1f
Caioscg <cacazaok5@gmail.com>
2023-05-31 21:13:49 -0300

    singIn

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -1,10 +1,26 @@
 import { createContext, useContext } from "react"
 
+import { api } from '../services/api'
+
 const AuthContext = createContext({})
 
 function AuthProvider({ children }) {
+    async function singIn({ email, password }) {
+        try {
+            const response = await api.post("/sessions", { email, password })
+            console.log(response)
+        } catch(error) {  // tratamento de excessões
+            if(error.response) {
+                alert(error.response.data.message)
+            }
+            else {
+                alert("Não foi possível entrar.")
+            }
+        }
+    }
+
     return(
-        <AuthContext.Provider value={{ name: "Caio Gonzaga", email: 'caio@email.com' }}>
+        <AuthContext.Provider value={{ singIn }}>
             {children}
         </AuthContext.Provider>
     )
